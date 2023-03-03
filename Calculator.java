5f45410cc6636a7f98999fba03771a9ab9a029ea
Akhilesh sharma <104143630+Akhileshsharma2121@users.noreply.github.com>
2023-03-03 12:29:28 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+package week1.day;
+
+public class Calculator {
+public static void main (String[] args) {
+	double a,b,subresult,divresult;
+	a = 340d;
+	b = 215.47d;
+	subresult = a-b;
+	divresult = a/b;
+	
+System.out.println("subresult="+subresult);
+System.out.println("divresult="+divresult);
+		
+	
+	
+}
+}
